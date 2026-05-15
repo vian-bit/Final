f04910f37da67e235be3208efbf07f49469b97b2
vian-bit <anakuocta@gmail.com>
2026-05-15 13:18:28 +0700

    reminder

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('attendance:send-checkin-reminder')
+    ->everyMinute()
+    ->timezone('Asia/Jakarta')
+    ->withoutOverlapping();
+
+Schedule::command('attendance:send-checkout-reminder')
+    ->everyMinute()
+    ->timezone('Asia/Jakarta')
+    ->withoutOverlapping();
+
+Schedule::command('attendance:send-daily-report')
+    ->dailyAt('17:00')
+    ->timezone('Asia/Jakarta')
+    ->withoutOverlapping();
